11c67658de56894fcbfa43d0ee2d40a437c3451d
behzad khosravifar <behzadkh@hotmail.com>
2019-11-23 19:27:42 +0330

    add paragraphBuilder class

---
+++ src/TextViewer/TextViewer.Sample/Reader/ParagraphBuilder.cs
@@ -0,0 +1,19 @@
+﻿using TextViewer;
+
+namespace TextViewerSample.Reader
+{
+    public static class ParagraphBuilder
+    {
+        public static Paragraph ConvertToParagraph(this IAtom atom)
+        {
+            var para = new Paragraph((int) atom.Offset);
+
+            foreach (var tag in atom.TagNodeList)
+            {
+                var styles = tag.Styles;
+            }
+
+            return para;
+        }
+    }
+}
